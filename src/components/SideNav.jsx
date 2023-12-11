d8bcc858e6a77395a066c894b925274be7e30eaa
Scriptkidd98 <duyilekan98@gmail.com>
2023-12-11 23:14:17 +0100

    changes made

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,15 +1,20 @@
 import { Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import { makeTabAccessible } from 'aria-ease'
 
 
 // eslint-disable-next-line react/prop-types
 const SideNav = ({page}) => {
+  useEffect(() => {
+    makeTabAccessible('side-nav-div', 'side-nav-link')
+  })
   return (
     <Col xs={0} sm={0} md={4} lg={3}>
       <div className='side-nav-div' id='side-nav-div'>
-        <Link to='/docs' className={(page === 'documentation') ? 'active-nav-link' : 'side-nav-link'}>Introduction</Link>
-        <Link to='/examples/menu' className={(page === 'menu') ? 'active-nav-link' : 'side-nav-link'}>Menu</Link>
-        <Link to='/examples/tab' className={(page === 'tab') ? 'active-nav-link' : 'side-nav-link'}>Tab</Link>
+        <Link to='/docs' className={`side-nav-link ${(page === 'documentation') ? 'active-nav-link' : ''}`}>Introduction</Link>
+        <Link to='/examples/menu' className={`side-nav-link ${(page === 'menu') ? 'active-nav-link' : ''}`}>Menu</Link>
+        <Link to='/examples/tab' className={`side-nav-link ${(page === 'tab') ? 'active-nav-link' : ''}`}>Tab</Link>
       </div>
     </Col>
   )
