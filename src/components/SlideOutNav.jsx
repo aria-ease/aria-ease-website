639a343755e34ec0f9966703d455d58c44341961
Scriptkidd98 <duyilekan98@gmail.com>
2023-12-12 00:22:38 +0100

    changes made

--- src/components/SlideOutNav.jsx
+++ src/components/SlideOutNav.jsx
@@ -5,16 +5,16 @@ import { Link } from "react-router-dom"
 const SlideOutNav = ({page}) => {
   return (
     <div className="slide-out-nav-outer-div">
-      <Link to='/' className={(page === 'home') ? 'active-nav-link' : 'side-nav-link'}>Home</Link>
+      <Link to='/' className={`side-nav-link ${(page === 'home') ? 'active-nav-link' : ''}`}>Home</Link>
       <div>
         <p>Documentation</p>
-        <Link to='/docs' className={(page === 'documentation') ? 'active-nav-link' : 'side-nav-link'}>Introduction</Link>
+        <Link to='/docs' className={`side-nav-link ${(page === 'documentation') ? 'active-nav-link' : ''}`}>Introduction</Link>
       </div>
       <div>
         <p>Components</p>
         <div className="slide-out-nav-div">
-          <Link to='/examples/menu' className={(page === 'menu') ? 'active-nav-link' : 'side-nav-link'}>Menu</Link>
-          <Link to='/examples/tab' className={(page === 'tab') ? 'active-nav-link' : 'side-nav-link'}>Tab</Link>
+          <Link to='/examples/menu' className={`side-nav-link ${(page === 'menu') ? 'active-nav-link' : ''}`}>Menu</Link>
+          <Link to='/examples/tab' className={`side-nav-link ${(page === 'tab') ? 'active-nav-link' : ''}`}>Tab</Link>
         </div>
       </div>
     </div>
