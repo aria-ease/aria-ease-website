43ea3b7734bc11eb14fd2de38b2c10f2af416ca9
Scriptkidd98 <duyilekan98@gmail.com>
2023-12-11 14:20:59 +0100

    changes made

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,17 +11,17 @@ const Header = ({darkMode, setDarkMode}) => {
     <div className="header">
         <span className="logo-span">aria-ease</span>
         <div className="header-search-div">
-            <svg fill="rgba(181, 181, 181, 1)" height="14" viewBox="0 0 13 14" width="13" xmlns="http://www.w3.org/2000/svg"><path clipRule="evenodd" d="m8.82264 10.3833c-.92307.7008-2.07429 1.1167-3.32264 1.1167-3.03757 0-5.5-2.46243-5.5-5.5s2.46243-5.5 5.5-5.5 5.5 2.46243 5.5 5.5c0 1.24835-.4159 2.39957-1.1167 3.32264l2.897 2.89706c.2929.2929.2929.7677 0 1.0606s-.7677.2929-1.0606 0zm.67736-4.3833c0 2.20914-1.79086 4-4 4s-4-1.79086-4-4 1.79086-4 4-4 4 1.79086 4 4z" fillRule="evenodd"/></svg>
+            <svg fill="rgba(181, 181, 181, 1)" height="18" viewBox="0 0 13 14" width="18" xmlns="http://www.w3.org/2000/svg"><path clipRule="evenodd" d="m8.82264 10.3833c-.92307.7008-2.07429 1.1167-3.32264 1.1167-3.03757 0-5.5-2.46243-5.5-5.5s2.46243-5.5 5.5-5.5 5.5 2.46243 5.5 5.5c0 1.24835-.4159 2.39957-1.1167 3.32264l2.897 2.89706c.2929.2929.2929.7677 0 1.0606s-.7677.2929-1.0606 0zm.67736-4.3833c0 2.20914-1.79086 4-4 4s-4-1.79086-4-4 1.79086-4 4-4 4 1.79086 4 4z" fillRule="evenodd"/></svg>
             <input type="text" placeholder="Search"/>
         </div>
         <div className='header-buttons-grid-div' id="header-buttons-grid-div">
             <a href="https://github.com/Scriptkidd98/aria-ease" className='header-button center-flex'><FaGithub className='header-button-icon'/></a>
             <button className='header-button center-flex' onClick={() => {setDarkMode(!darkMode)}}>
                 {darkMode ? 
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="Sun">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="Sun" height="18" width="18">
                         <path d="M5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12ZM5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM12,5a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5Zm5.66,2.34a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34Zm-12-.29a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5ZM12,19a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19Z" fill="#c0c0c0" className="color000000 svgShape"></path>
                     </svg> : 
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" id="Moon">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" id="Moon" height="18" width="18">
                         <g fill="#000000" className="color000000 svgShape">
                         <g fill="#000000" className="color000000 svgShape">
                         <path fill="#808080" d="M40.8 28.4C43.5 18 38.1 7 28.2 2.7c-2.6-1.1-5.4-1.7-8-1.8-.9 0-1.4 1-.8 1.7 4.5 4.7 6.8 11 5.8 17.8-.9 3.6-6.2 2.7-8 4.4-1.9 1.8 4.2 5.1 4.2 5.1-3.3 4.7-8.5 8.2-14.1 9.4-.9.2-1.1 1.3-.3 1.8 1.7 1.2 3.6 2.1 5.6 2.8 12 3.9 24.9-3.1 28.2-15.5z" className="colorffe500 svgShape"></path>
