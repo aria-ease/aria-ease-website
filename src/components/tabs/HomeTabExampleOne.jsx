02f3cc69c0b1fe58cecdf40ae6cc75325ee2dea8
Scriptkidd98 <duyilekan98@gmail.com>
2023-12-16 13:50:22 +0100

    changes made

--- src/components/tabs/HomeTabExampleOne.jsx
+++ src/components/tabs/HomeTabExampleOne.jsx
@@ -1,16 +1,10 @@
-import { makeBlockAccessible } from "aria-ease"
-import { useEffect } from "react"
-
 const HomeTabExampleOne = () => {
-    useEffect(() => {
-        makeBlockAccessible('home-example-tab-one', 'home-example-tab-one-item')
-    })
   return (
     <div className="home-example-tab-one" id="home-example-tab-one">
-        <button className="home-example-tab-one-item" onClick={() => alert('Button clicked')}>One</button>
-        <button className="home-example-tab-one-item" onClick={() => alert('Button clicked')}>Two</button>
-        <button className="home-example-tab-one-item" onClick={() => alert('Button clicked')}>Three</button>
-        <button className="home-example-tab-one-item" onClick={() => alert('Button clicked')}>Four</button>
+        <button className="home-example-tab-one-item block-interactive" onClick={() => alert('Button clicked')}>One</button>
+        <button className="home-example-tab-one-item block-interactive" onClick={() => alert('Button clicked')}>Two</button>
+        <button className="home-example-tab-one-item block-interactive" onClick={() => alert('Button clicked')}>Three</button>
+        <button className="home-example-tab-one-item block-interactive" onClick={() => alert('Button clicked')}>Four</button>
     </div>
   )
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,6 +4,7 @@ import HomeExampleMenu from "../components/menus/HomeExampleMenu"
 import HomeTabExampleOne from "../components/tabs/HomeTabExampleOne"
 import SlideOutNav from "../components/SlideOutNav"
 import { useState, useEffect } from "react"
+import { makeBlockAccessible } from "aria-ease"
 
 // eslint-disable-next-line react/prop-types
 const Homepage = ({darkMode, setDarkMode}) => {
@@ -18,32 +19,37 @@ const Homepage = ({darkMode, setDarkMode}) => {
     }
   },[showDropdownPage])
 
+  useEffect(() => {
+    makeBlockAccessible('inner-body-div', 'block-interactive')
+  },[])
+
   return (
-    <div className="home-body">
-        <Header darkMode={darkMode} setDarkMode={setDarkMode} showDropdownPage={showDropdownPage} setShowDropdownPage={setShowDropdownPage}/>
+    <div className="home-body" id="inner-body-div">
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} showDropdownPage={showDropdownPage} setShowDropdownPage={setShowDropdownPage}/>
 
-        <div className="page-body-div">
-          <Container fluid>
-            <Row>
-              <Col xs={12} sm={12} md={12} lg={6}>
-                <div className="hero-text-div">
-                  <h1 className="hero-heading">Add accessibility to your components.</h1>
-                  <p className="hero-paragraph">Out-of-the-box accessibility utility package that you can add to your components with a single function. Automate menu and block focus trapping, keyboard interactions, aria attributes update, and more. Open source.</p>
-                </div>
-              </Col>
-              <Col xs={0} sm={0} md={0} lg={6} style={{paddingRight: '0px'}}>
-                <div className="hero-examples-div">
-                  <HomeExampleMenu/>
-                  <HomeTabExampleOne/>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
+      <div className="page-body-div">
+        <Container fluid>
+          <Row>
+            <Col xs={12} sm={12} md={12} lg={6}>
+              <div className="hero-text-div">
+                <h1 className="hero-heading">Add accessibility to your components.</h1>
+                <p className="hero-paragraph">Out-of-the-box accessibility utility package that you can add to your components with a single function. Automate menu and block focus trapping, keyboard interactions, aria attributes update, and more. Open source.</p>
+              </div>
+            </Col>
+            <Col xs={0} sm={0} md={0} lg={6} style={{paddingRight: '0px'}}>
+              <div className="hero-examples-div">
+                <HomeExampleMenu/>
+                <HomeTabExampleOne/>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+        
 
-        <div className={`slide-out-side-nav-div ${showDropdownPage ? 'slide-in' : ''}`}>
-          <SlideOutNav page={page}/>
-        </div>
+      <div className={`slide-out-side-nav-div ${showDropdownPage ? 'slide-in' : ''}`}>
+        <SlideOutNav page={page}/>
+      </div>
     </div>
   )
 }
