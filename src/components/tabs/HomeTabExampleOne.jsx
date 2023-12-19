a913d19d0cf85c56d7834ff1053a1a2babe4c166
Scriptkidd98 <duyilekan98@gmail.com>
2023-12-19 12:23:52 +0100

    changes made

--- src/components/tabs/HomeTabExampleOne.jsx
+++ src/components/tabs/HomeTabExampleOne.jsx
@@ -1,10 +1,18 @@
+import { useEffect } from "react"
+import { makeBlockAccessible } from "aria-ease"
+
 const HomeTabExampleOne = () => {
+
+  useEffect(() => {
+    makeBlockAccessible('home-example-tab-one', 'home-example-tab-one-item')
+  },[])
+
   return (
     <div className="home-example-tab-one" id="home-example-tab-one">
-        <button className="home-example-tab-one-item block-interactive" onClick={() => alert('Button clicked')}>One</button>
-        <button className="home-example-tab-one-item block-interactive" onClick={() => alert('Button clicked')}>Two</button>
-        <button className="home-example-tab-one-item block-interactive" onClick={() => alert('Button clicked')}>Three</button>
-        <button className="home-example-tab-one-item block-interactive" onClick={() => alert('Button clicked')}>Four</button>
+        <button className="home-example-tab-one-item" onClick={() => alert('Button clicked')}>One</button>
+        <button className="home-example-tab-one-item" onClick={() => alert('Button clicked')}>Two</button>
+        <button className="home-example-tab-one-item" onClick={() => alert('Button clicked')}>Three</button>
+        <button className="home-example-tab-one-item" onClick={() => alert('Button clicked')}>Four</button>
     </div>
   )
 }
