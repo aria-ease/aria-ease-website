1afe0ee4c33680422413c0f627db9d57ee2fbdb5
Scriptkidd <duyilekan98@gmail.com>
2023-12-26 12:56:27 +0100

    changes made

--- src/components/menus/HomeExampleMenu.jsx
+++ src/components/menus/HomeExampleMenu.jsx
@@ -4,16 +4,16 @@ const HomeExampleMenu = () => {
     const toggleMenuDisplay = (event) => {
       if(event.key === 'Enter' || event.key === " ") {
         event.preventDefault();
-        const menu = document.querySelector('#custom-menu')
-        if(getComputedStyle(menu).display === 'none') {
-          menu.style.display = 'block'
-          makeMenuAccessible('custom-menu', 'profile-menu-item');
-          updateMenuTriggerAriaAttributes('display-button', 'Hide profile menu')
-        } else {
-          cleanUpMenuEventListeners('custom-menu', 'profile-menu-item')
-          menu.style.display = 'none'
-          updateMenuTriggerAriaAttributes('display-button', 'Display profile menu')
-        }
+      }
+      const menu = document.querySelector('#custom-menu')
+      if(getComputedStyle(menu).display === 'none') {
+        menu.style.display = 'block'
+        makeMenuAccessible('custom-menu', 'profile-menu-item');
+        updateMenuTriggerAriaAttributes('display-button', 'Hide profile menu')
+      } else {
+        cleanUpMenuEventListeners('custom-menu', 'profile-menu-item')
+        menu.style.display = 'none'
+        updateMenuTriggerAriaAttributes('display-button', 'Display profile menu')
       }
     }
   return (
