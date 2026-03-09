5000c10585799f179673f6c8f5088d17d3a65c0a
Scriptkid98 <duyilekan98@gmail.com>
2026-03-09 21:04:00 +0100

    changes made

--- tests/ariaease.test.jsx
+++ tests/ariaease.test.jsx
@@ -5,32 +5,32 @@ afterAll(async () => {
   await cleanupTests();
 });
 
-describe("Accordion Accessibility Test", () => {
+describe("Accordion WAI-ARIA Accessibility Test", () => {
   test("renders Accordion without accessibility violation(s)", async () => {
     await testUiComponent("accordion", null, "http://localhost:5173/test-harness?component=accordion");
   });
 });
 
-describe("Combobox Accessibility Test", () => {
-  test("renders Combobox without accessibility violation(s)", async () => {
+describe("Combobox WAI-ARIA Accessibility Test", () => {
+  test("Combobox meets WAI-ARIA roles, states, properties, and keyboard interactions expectactions", async () => {
     await testUiComponent("combobox", null, "http://localhost:5173/test-harness?component=combobox");
   });
 });
 
-describe("Menu Accessibility Test", () => {
-  test("renders Menu without accessibility violation(s)", async () => {
+describe("Menu WAI-ARIA Accessibility Test", () => {
+  test("Menu meets WAI-ARIA roles, states, properties, and keyboard interactions expectactions", async () => {
     await testUiComponent("menu", null, "http://localhost:5173/test-harness?component=menu");
   });
 });
 
-describe("Horizontal Tabs Accessibility Test", () => {
-  test("renders horizontal tabs without accessibility violation(s)", async () => {
+describe("Horizontal Tabs WAI-ARIA Accessibility Test", () => {
+  test("Horizontal tabs meets WAI-ARIA roles, states, properties, and keyboard interactions expectactions", async () => {
     await testUiComponent("tabs", null, "http://localhost:5173/test-harness?component=tabs_horizontal");
   });
 });
 
-describe("Vertical Tabs Accessibility Test", () => {
-  test("renders vertical tabs without accessibility violation(s)", async () => {
+describe("Vertical Tabs WAI-ARIA Accessibility Test", () => {
+  test("Vertical tabs meets WAI-ARIA roles, states, properties, and keyboard interactions expectactions", async () => {
     await testUiComponent("tabs", null, "http://localhost:5173/test-harness?component=tabs_vertical");
   });
 });
